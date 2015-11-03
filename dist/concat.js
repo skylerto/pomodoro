b533d50863bb881f519dcc61817e122fca3f9fa3
Skyler Layne <skylerclayne@gmail.com>
2015-11-03 13:29:46 -0500

    renamed some files to keep test passing

--- dist/concat.js
+++ dist/concat.js
@@ -10,35 +10,6 @@
     this[name] = definition();
   };
 
-}('Break', function() {
-  'use strict';
-  /*
-   * Facade pattern for interfacing with the timer.js class.
-   */
-
-  /* Bind the elements to view elements off the bat */
-  function Break(minutesView, secondsView, pomodoroCounter) {
-    var duration = 5 * 60;
-    var pomodoro = new Pomodoro(duration, minutesView, secondsView, pomodoroCounter);
-  };
-
-  Break.prototype = Object.create(Pomodoro.prototype);
-
-  return Break;
-}));
-
-(function(name, definition) {
-  var hasDefine = typeof define === 'function',
-    hasExports = typeof module !== 'undefined' && module.exports;
-
-  if (hasDefine) {
-    define(name, definition);
-  } else if (hasExports) {
-    module.exports = definition();
-  } else {
-    this[name] = definition();
-  };
-
 }('Pomodoro', function() {
   'use strict';
   /*
@@ -139,33 +110,4 @@
   }
 
   return Pomodoro;
-}));
-
-(function(name, definition) {
-  var hasDefine = typeof define === 'function',
-    hasExports = typeof module !== 'undefined' && module.exports;
-
-  if (hasDefine) {
-    define(name, definition);
-  } else if (hasExports) {
-    module.exports = definition();
-  } else {
-    this[name] = definition();
-  };
-
-}('Task', function() {
-  'use strict';
-  /*
-   * Facade pattern for interfacing with the timer.js class.
-   */
-
-  /* Bind the elements to view elements off the bat */
-  function Task(minutesView, secondsView, pomodoroCounter) {
-    var duration = 25 * 60;
-    var pomodoro = new Pomodoro(duration, minutesView, secondsView, pomodoroCounter);
-  };
-
-  Task.prototype = Object.create(Pomodoro.prototype);
-
-  return Task;
 }));
