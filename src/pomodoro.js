0f5f75a054c6fcf0fdc87665e7cad5f0228e334e
Skyler Layne <skylerclayne@gmail.com>
2016-04-13 10:41:26 -0400

    yuidoc in pomodoro

--- src/pomodoro.js
+++ src/pomodoro.js
@@ -1,6 +1,22 @@
 const Timer = require('./timer.js');
 
+/**
+ * This class manages 2 timers, a break timer and a task timer.
+ *
+ * @class Pomodoro
+ * @constructor
+ */
 module.exports = class Pomodoro {
+
+  /**
+   * Contructor to create a new Pomodoro. Involked with new.
+   *
+   * @method constructor
+   * @param {Int} [taskTime] The time in minutes to instantiate the taskTimer with.
+   * @param {Int} [breakTime] The time in minutes to instantiate the breakTimer
+   * with.
+   * @return {Pomodoro} a new Pomodoro object.
+   */
   constructor(taskTime, breakTime) {
     if (!taskTime) {
       this._taskTimer = new Timer();
@@ -14,19 +30,43 @@ module.exports = class Pomodoro {
     }
   }
 
+  /**
+   *  Get the taskTimer.
+   *  Involked by instance.taskTimer.
+   *
+   * @method taskTimer
+   * @return {Timer} a Timer instance representing the taskTimer.
+   */
   get taskTimer() {
     return this._taskTimer;
   }
 
+  /**
+   * Get the breakTimer.
+   * Involked by instance.breakTimer.
+   *
+   * @method breakTimer
+   * @return {Timer} a Timer instance representing the breakTimer.
+   */
   get breakTimer() {
     return this._breakTimer;
   }
 
+  /**
+   * Start the taskTimer.
+   *
+   * @method startTask
+   */
   startTask() {
     this._taskTimer.start();
     this._breakTimer.reset();
   }
 
+  /**
+   * Start the breakTimer, on the condition that the taskTimer has completed.
+   *
+   * @method startBreak
+   */
   startBreak() {
     if (this.taskTimer.duration === 0) {
       this._breakTimer.start();
