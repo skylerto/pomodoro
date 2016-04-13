16699687ccd9e39dedffec58b09489eab05ada8d
Skyler Layne <skylerclayne@gmail.com>
2016-04-13 14:46:47 -0400

    update docs

---
+++ out/api.js
@@ -0,0 +1,11 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Pomodoro",
+        "Timer"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});
