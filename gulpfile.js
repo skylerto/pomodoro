f27f95abe769fce226fc582a85a1ae36ebd20dbb
Skyler Layne <skylerclayne@gmail.com>
2016-04-13 14:46:47 -0400

    update docs

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-const gulp = require('gulp');
-const concat = require('gulp-concat');
-
-gulp.task('concat', () => gulp.src(['build/timer.js', 'build/pomodoro.js'])
-                              .pipe(concat('pomodoro.js'))
-                              .pipe(gulp.dest('dist'))
-         );
